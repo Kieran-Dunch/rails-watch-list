8fa7ecf87ffa45080843314f3601150b7d8bf608
Kieran Dunch <dunch.kieran@gmail.com>
2022-02-17 16:21:23 -0500

    Okay, now we are cooking with gas

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,36 @@
 class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: [:create, :destroy]
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to list_path(@bookmark.list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    bookmark = set_bookmark
+    bookmark.delete
+    redirect_to list_path(bookmark.list)
+  end
+
+  private
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def bookmark_params
+    params.require(:list).permit(:name)
+  end
 end
